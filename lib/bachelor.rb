cc7bdeea1a80700eee578b91ed192be898988914
Zoncharne Lee <zoncharne@gmail.com>
2019-03-11 03:23:16 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -45,5 +45,12 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age = []
+  number_of_age = 0
+  data[season].each do |season_hash|
+    age << season_hash["age"]
+    number_of_age += season_hash["age"].to_i
+  end
+  average = (number_of_age.to_f / age.size).round
+  average
 end
